70418b5e4b09e1f7db295dbab7662288299366e5
MishaKoval <62940078+MishaKoval@users.noreply.github.com>
2021-05-07 22:16:05 +0300

    updated main with config and argparse

--- lab2/Serializers/main.py
+++ lab2/Serializers/main.py
@@ -1,6 +1,9 @@
+import configparser
 from Serializers.MySerializer import MySerializer
 from Serializers.SerializerFactory import Factory
 from Serializers.SerializerFactory import SerializeEnum
+from pathlib import Path
+import argparse
 
 
 def gettype(type):
@@ -18,9 +21,44 @@ def gettype(type):
     return serializer
 
 
+def serialize(dest_format, path_file):
+    serializer = gettype(dest_format)
+    try:
+        src_format = Path(path_file).suffix
+        src_format = src_format.removeprefix('.')
+        src_format = src_format.upper()
+        if src_format == dest_format:
+            return
+        deserializer = gettype(src_format)
+        abs_path = Path(path_file)
+        loaded = deserializer.load(path_file)
+        dest_format = '.' + dest_format
+        dest_format = dest_format.lower()
+        serializer.dump(loaded, Path(abs_path.parent, f"{abs_path.stem}{dest_format}"))
+    except FileNotFoundError:
+        print("wrong path or format")
+
+
 def main():
-    gettype("JSON").dump(gettype("TOML").load("./Files/file.toml"), "./Files/file.json")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", dest="config_file", help="Path for the configuration file")
+    parser.add_argument("-f", "--format", dest="dest_format", help="New file format")
+    parser.add_argument("-p", "--path", dest="path_file", help="Path for the input file")
+    args = parser.parse_args()
+
+    if args.config_file is not None:
+        config = configparser.ConfigParser()
+        try:
+            config.read(args.config_file)
+            serialize(config["settings"]["dest_format"], config["settings"]["path_file"])
+        except KeyError:
+            print("invalid file")
+    else:
+        if args.dest_format and args.path_file:
+            serialize(args.dest_format, args.path_file)
+        else:
+            raise TypeError("invalid parameters")
 
 
-if __name__ == '__main__':
-    main()
+main()
