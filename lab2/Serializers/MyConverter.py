9a830636db3b02abdc43a1d5ecf97463aa0d9981
MishaKoval <62940078+MishaKoval@users.noreply.github.com>
2021-05-08 04:22:43 +0300

    Some fixes in Converter

--- lab2/Serializers/MyConverter.py
+++ lab2/Serializers/MyConverter.py
@@ -6,76 +6,27 @@ import builtins
 
 class Converter:
 
-    @staticmethod
-    def __is_default(obj):
-        return type(obj) in [int, float, str, bool, type(None)]
+    def is_default(self, obj):
+        return type(obj) in [int, float, str, bool, type(None), list, tuple]
 
-    def object_dictionary(self, obj):
-        if self.__is_default(obj):
-            return obj
-        elif isinstance(obj, list):
-            lst = list()
-            for item in obj:
-                lst.append(self.object_dictionary(item))
-            return lst
-        elif isinstance(obj, dict):
-            return self.__to_dictionary(obj)
-        else:
-            return self.__to_dictionary(obj.__dict__)
-
-    def __to_dictionary(self, dict_obj):
-        obj_dict = dict()
-        for (key, value) in dict_obj.items():
-            if self.__is_default(value):
-                obj_dict[key] = value
-            else:
-                obj_dict[key] = self.__to_dictionary(value)
-
-        return obj_dict
-
-    def dictionary_object(self, dict_obj):
-        if self.__is_default(dict_obj):
+    def dict_to_obj(self, dict_obj):
+        if self.is_default(dict_obj):
             return dict_obj
         elif isinstance(dict_obj, list):
             lst = list()
             for item in dict_obj:
-                lst.append(self.object_dictionary(item))
+                lst.append(self.dict_to_obj(item))
             return lst
-
         else:
             temp = dict()
             for (key, value) in dict_obj.items():
-                if not self.__is_default(value):
-                    temp[key] = self.object_dictionary(value)
+                if not self.is_default(value):
+                    temp[key] = self.dict_to_obj(value)
                 else:
                     temp[key] = value
-
         return namedtuple('object', temp.keys())(*temp.values())
 
-    def function_dictionary(self, func_obj):
-        func = dict()
-
-        function_code = dict()
-        code_pairs = inspect.getmembers(func_obj.__code__)
-        for (key, value) in code_pairs:
-            if str(key).startswith('co_'):
-                if isinstance(value, bytes):
-                    value = (list(value))
-                function_code[key] = value
-        func['code'] = function_code
-
-        function_globals = dict()
-        name = func['code']['co_name']
-        function_globals[name] = name + '<function>'
-        global_pairs = func_obj.__globals__.items()
-        for (key, value) in global_pairs:
-            if self.__is_default(value):
-                function_globals[key] = value
-        func['globals'] = function_globals
-
-        return func
-
-    def dictionary_function(self, dict_func):
+    def dict_to_func(self, dict_func):
         function_globals = dict_func['globals']
         function_globals['__builtins__'] = builtins
         code_args = dict_func['code']
@@ -101,3 +52,47 @@ class Converter:
         name = code_args['co_name']
         function_globals[name] = temp
         return FunctionType(function_code, function_globals, name)
+
+    def obj_to_dict(self, obj):
+        if self.is_default(obj):
+            return obj
+        elif isinstance(obj, list):
+            lst = list()
+            for item in obj:
+                lst.append(self.obj_to_dict(item))
+            return lst
+        elif isinstance(obj, dict):
+            return self.to_dictionary(obj)
+        else:
+            return self.to_dictionary(obj.__dict__)
+
+    def func_to_dict(self, func_obj):
+        func = dict()
+        function_code = dict()
+        code_pairs = inspect.getmembers(func_obj.__code__)
+        for (key, value) in code_pairs:
+            if str(key).startswith('co_'):
+                if isinstance(value, bytes):
+                    value = (list(value))
+                function_code[key] = value
+        func['code'] = function_code
+
+        function_globals = dict()
+        name = func['code']['co_name']
+        function_globals[name] = name + '<function>'
+        global_pairs = func_obj.__globals__.items()
+        for (key, value) in global_pairs:
+            if self.is_default(value):
+                function_globals[key] = value
+        func['globals'] = function_globals
+
+        return func
+
+    def to_dictionary(self, dict_obj):
+        obj_dict = dict()
+        for (key, value) in dict_obj.items():
+            if self.is_default(value):
+                obj_dict[key] = value
+            else:
+                obj_dict[key] = self.obj_to_dict(value)
+        return obj_dict
